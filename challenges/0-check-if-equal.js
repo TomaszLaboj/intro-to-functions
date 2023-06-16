f271fb64c29ccf092c53d70fe43e6c74f75605e0
TomaszLaboj <tomasz.laboj@gmail.com>
2023-06-16 19:44:56 +0100

    changes changes

---
+++ challenges/0-check-if-equal.js
@@ -0,0 +1,13 @@
+function isEqual(numOne,numTwo){
+    if(numOne == numTwo){
+        return true;
+    }
+
+    return false;
+}
+
+console.log(isEqual(), 'should be', )
+console.log(isEqual(), 'should be', )
+console.log(isEqual(), 'should be', )
+console.log(isEqual(), 'should be', )
+console.log(isEqual(), 'should be', )
