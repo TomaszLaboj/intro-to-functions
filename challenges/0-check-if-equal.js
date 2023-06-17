c8e2aa6048ec336f2d2de78fe5504d79252ed5df
TomaszLaboj <tomasz.laboj@gmail.com>
2023-06-17 12:16:16 +0100

    changed test cases

--- challenges/0-check-if-equal.js
+++ challenges/0-check-if-equal.js
@@ -1,13 +1,13 @@
 function isEqual(numOne,numTwo){
-    if(numOne == numTwo){
+    if(numOne === numTwo){
         return true;
     }
 
     return false;
 }
 
-console.log(isEqual(), 'should be', )
-console.log(isEqual(), 'should be', )
-console.log(isEqual(), 'should be', )
-console.log(isEqual(), 'should be', )
-console.log(isEqual(), 'should be', )
+console.log(isEqual(3,3), 'should be', true )
+console.log(isEqual(3,4), 'should be', false )
+// console.log(isEqual(), 'should be', )
+// console.log(isEqual(), 'should be', )
+// console.log(isEqual(), 'should be', )
